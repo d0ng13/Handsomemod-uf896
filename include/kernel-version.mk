579adf203d5ab96af675766d27f244ac6cd55c46
dreirund <1590519+dreirund@users.noreply.github.com>
2022-09-26 12:48:31 +0200

    Prepared `include/kernel-version.mk` for kernel versions 5.4.214, 5.10.10 and 5.10.145. But not switched to them because kernel patches would fail to apply.

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,15 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .203
+# LINUX_VERSION-5.4 = .214
 LINUX_VERSION-5.10 = .33
+# LINUX_VERSION-5.10 = .145
 
 LINUX_KERNEL_HASH-5.4.203 = fc933f5b13066cfa54aacb5e86747a167bad1d8d23972e4a03ab5ee36c29798a
+LINUX_KERNEL_HASH-5.4.214 = 9abc79b4808c827eb82f8a44313ce6d3ade255949989bb98c11acda7ca3bc5db
+LINUX_KERNEL_HASH-5.10.10 = 60ed866fa951522a5255ea37ec3ac2006d3f3427d4783a13ef478464f37cdb19
 LINUX_KERNEL_HASH-5.10.33 = 933fdbc36371c0f830b7a6a957a559fca2dad1cc0eaa852ef42fb168185b4315
+LINUX_KERNEL_HASH-5.10.145 = fc5990f3e57479369eaf193bd7760b84f26d2b670c2d88e473101cb679c5ac61
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
